8ddcaa88eb3339fd3b552b1364558757486bb325
Lester <lesterdlb@gmail.com>
2023-02-13 13:26:01 -0600

    Add infrastructure DI

--- src/AngularProductsCRUD.Api/Program.cs
+++ src/AngularProductsCRUD.Api/Program.cs
@@ -1,25 +1,24 @@
+using AngularProductsCRUD.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddInfrastructure();
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    app.UseExceptionHandler("/error");
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
---
+++ src/AngularProductsCRUD.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using AngularProductsCRUD.Application.Common.Interfaces.Persistence;
+using AngularProductsCRUD.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AngularProductsCRUD.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("AngularCRUD"));
+
+        services.AddScoped(typeof(GenericRepository<>), typeof(GenericRepository<>));
+        services.AddScoped<IProductsRepository, ProductsRepository>();
+        services.AddScoped<ICategoriesRepository, CategoriesRepository>();
+
+        return services;
+    }
+}
