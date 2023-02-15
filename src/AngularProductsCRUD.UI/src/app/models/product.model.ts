40116413077a98e795df0ed39fd95780bb8941fa
Lester <lesterdlb@gmail.com>
2023-02-14 19:11:38 -0600

    Add products-list component

---
+++ src/AngularProductsCRUD.UI/src/app/models/product.model.ts
@@ -0,0 +1,11 @@
+﻿export interface Product {
+    id: string;
+    title: string;
+    categoryId: string;
+    category: string;
+    price: number;
+}
+
+export interface ProductRequest
+    extends Pick<Product, 'title' | 'categoryId' | 'price'> {
+}
--- src/AngularProductsCRUD.UI/src/app/services/products.service.ts
+++ src/AngularProductsCRUD.UI/src/app/services/products.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductsService {
+    private readonly apiUrl = 'https://localhost:7036/api/products';
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    getProducts(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.apiUrl);
+    }
+
+    getProduct(id: string): Observable<Product> {
+        return this.http.get<Product>(`${this.apiUrl}/${id}`);
+    }
+
+    addProduct(product: Product): Observable<Product> {
+        return this.http.post<Product>(this.apiUrl, product);
+    }
+
+    editProduct(product: Product): Observable<Product> {
+        return this.http.put<Product>(`${this.apiUrl}/${product.id}`, product);
+    }
+
+    deleteProduct(id: string): Observable<Product> {
+        return this.http.delete<Product>(`${this.apiUrl}/${id}`);
+    }
 }
