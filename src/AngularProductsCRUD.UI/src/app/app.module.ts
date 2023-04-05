86d8e174f2e50ba921d66a441c029966e349cbf5
Lester <lesterdlb@gmail.com>
2023-04-05 12:50:23 -0600

    Refactor edit product component

--- src/AngularProductsCRUD.UI/src/app/app.module.ts
+++ src/AngularProductsCRUD.UI/src/app/app.module.ts
@@ -1,32 +1,23 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
+
 import {AppComponent} from './app.component';
-import {CategoriesListComponent} from './components/categories/categories-list/categories-list.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ReactiveFormsModule} from '@angular/forms';
-import { AddCategoryComponent } from './components/categories/add-category/add-category.component';
-import { ProductsListComponent } from './components/products/products-list/products-list.component';
-import { EditCategoryComponent } from './components/categories/edit-category/edit-category.component';
-import { AddProductComponent } from './components/products/add-product/add-product.component';
-import { EditProductComponent } from './components/products/edit-product/edit-product.component';
+import {NavComponent} from './nav/nav.component';
+
+import {CategoryModule} from './category/category.module';
+import {ProductModule} from './product/product.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        CategoriesListComponent,
-        AddCategoryComponent,
-        ProductsListComponent,
-        EditCategoryComponent,
-        AddProductComponent,
-        EditProductComponent
+        NavComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule
+        CategoryModule,
+        ProductModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/AngularProductsCRUD.UI/src/app/product/edit/edit.component.ts
@@ -0,0 +1,99 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Category} from '../../models/category.model';
+import {CategoriesService} from '../../services/categories.service';
+import {ProductsService} from '../../services/products.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Product} from '../../models/product.model';
+import {HttpErrorResponse} from '@angular/common/http';
+import {FormBuilderService} from '../../services/form-builder.service';
+
+@Component({
+    selector: 'app-edit',
+    templateUrl: './edit.component.html',
+    styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+    public productForm: FormGroup = new FormGroup({});
+    private categoriesList: Category[] = [];
+
+    constructor(
+        private formBuilderService: FormBuilderService,
+        private categoriesService: CategoriesService,
+        private productsService: ProductsService,
+        private route: ActivatedRoute,
+        private router: Router) {
+    }
+
+    get id() {
+        return this.productForm.get('id') as FormControl;
+    }
+
+    get title() {
+        return this.productForm.get('title') as FormControl;
+    }
+
+    get categoryId() {
+        return this.productForm.get('categoryId') as FormControl;
+    }
+
+    get price() {
+        return this.productForm.get('price') as FormControl;
+    }
+
+    get categories(): Category[] {
+        return this.categoriesList;
+    }
+
+    ngOnInit(): void {
+        this.productForm = this.formBuilderService.createProductForm();
+
+        this.categoriesService.getCategories().subscribe({
+            next: categories => this.categoriesList = categories,
+            error: error => console.log(error)
+        });
+
+        this.route.paramMap.subscribe({
+            next: params => {
+                const id = params.get('id');
+                if (id) {
+                    this.productsService.getProduct(id).subscribe({
+                        next: product => this.productForm.setValue({...product}),
+                        error: error => console.log(error)
+                    });
+                }
+            },
+            error: error => console.log(error)
+        });
+    }
+
+    editProduct(): void {
+        if (this.productForm.valid) {
+            this.productsService
+                .editProduct(this.productForm.value as Product)
+                .subscribe({
+                    next: (_) => this.router.navigate(['/products']),
+                    error: (error: HttpErrorResponse) => {
+                        Object.entries(error.error.errors).forEach(([_, value]) => {
+                            // this.apiError = value as string;
+                        });
+                    }
+                });
+        }
+    }
+
+    deleteProduct(): void {
+        if (confirm('Are you sure you want to delete this product?')) {
+            this.productsService
+                .deleteProduct(this.productForm.value.id)
+                .subscribe({
+                    next: (_) => this.router.navigate(['/products']),
+                    error: (error: HttpErrorResponse) => {
+                        Object.entries(error.error.errors).forEach(([_, value]) => {
+                            // this.apiError = value as string;
+                        });
+                    }
+                });
+        }
+    }
+}
