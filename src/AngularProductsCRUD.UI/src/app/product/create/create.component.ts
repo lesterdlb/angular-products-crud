9255c99b1812df399de76c5acf9ceb56a5827f8c
Lester <lesterdlb@gmail.com>
2023-04-05 12:50:05 -0600

    Refactor create product component

---
+++ src/AngularProductsCRUD.UI/src/app/product/create/create.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ProductsService} from '../../services/products.service';
+import {ProductRequest} from '../../models/product.model';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Category} from '../../models/category.model';
+import {CategoriesService} from '../../services/categories.service';
+import {FormBuilderService} from '../../services/form-builder.service';
+
+@Component({
+    selector: 'app-create',
+    templateUrl: './create.component.html',
+    styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+    productForm: FormGroup = new FormGroup({});
+    private categoriesList: Category[] = [];
+
+    constructor(
+        private formBuilderService: FormBuilderService,
+        private productsService: ProductsService,
+        private categoriesService: CategoriesService,
+        private router: Router) {
+    }
+
+    get title() {
+        return this.productForm.get('title') as FormControl;
+    }
+
+    get price() {
+        return this.productForm.get('price') as FormControl;
+    }
+
+    get categories(): Category[] {
+        return this.categoriesList;
+    }
+
+    ngOnInit(): void {
+        this.productForm = this.formBuilderService.createProductForm();
+
+        this.categoriesService.getCategories().subscribe({
+            next: categories => this.categoriesList = categories,
+            error: error => console.log(error)
+        });
+    }
+
+    addProduct(): void {
+        if (this.productForm.valid) {
+            this.productsService
+                .addProduct(this.productForm.value as ProductRequest)
+                .subscribe({
+                    next: (_) => {
+                        return this.router.navigate(['/products']);
+                    },
+                    error: (error: HttpErrorResponse) => {
+                        Object.entries(error.error.errors).forEach(([_, value]) => {
+                            // this.apiError = value as string;
+                        });
+                    }
+                });
+        }
+    }
+}
