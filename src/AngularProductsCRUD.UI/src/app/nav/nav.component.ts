f49d350c03a793702e963fb8721c315318fbc614
Lester <lesterdlb@gmail.com>
2023-04-08 13:09:37 -0600

    Update nav to reflect selected link

--- src/AngularProductsCRUD.UI/src/app/nav/nav.component.ts
+++ src/AngularProductsCRUD.UI/src/app/nav/nav.component.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core';
 
+interface NavItem {
+    label: string;
+    route: string;
+}
+
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
     styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+    public navItems: NavItem[] = [
+        {label: 'Products', route: '/products'},
+        {label: 'Categories', route: '/categories'}
+    ];
 }
